d94b4c2131d7f1afd09c7602d0ab78098be020ec
Dan Cuomo <dcuomo@outlook.com>
2021-02-18 09:02:04 -0800

    Test Commit

--- reload.ps1
+++ reload.ps1
@@ -1,3 +1,5 @@
+# This file is to help reload the nested modules if a change was made.
+
 remove-module helpers -Force -ErrorAction SilentlyContinue
 remove-module datatypes -Force -ErrorAction SilentlyContinue
 remove-module Test-NicProperties -Force -ErrorAction SilentlyContinue
