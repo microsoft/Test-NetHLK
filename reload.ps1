c5c0aa58fe2e7fcec456d68f5f5223a5a90a03a7
Dan Cuomo <dcuomo@outlook.com>
2021-02-24 15:16:11 -0800

    Removing reload

--- reload.ps1
+++
@@ -1,7 +0,0 @@
-# This file is to help reload the nested modules if a change was made.
-
-remove-module helpers -Force -ErrorAction SilentlyContinue
-remove-module datatypes -Force -ErrorAction SilentlyContinue
-remove-module Test-NicProperties -Force -ErrorAction SilentlyContinue
-
-ipmo .\Test-NICProperties.psd1 -Force
